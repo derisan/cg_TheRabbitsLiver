6cb6b3fdee4b763256a221b9d88f19901211e213
derisan <derisan@naver.com>
2020-11-12 21:03:37 +0900

    Tree class 추가

---
+++ TheRabbitsLiver/tree.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "actor.h"
+class Tree :
+    public Actor
+{
+public:
+    enum TreeType
+    {
+        kBasic
+    };
+
+    Tree(class Gfw* gfw, TreeType type, Gfw::Layer layer = Gfw::Layer::kTree);
+
+    // Getters
+    class BoxComponent* GetBox() const { return mBox; }
+    TreeType GetType() const { return mType; }
+
+private:
+    class BoxComponent* mBox;
+
+    TreeType mType;
+};
+
